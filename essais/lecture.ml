b8906dc0062b53fe0e8d000f9cdfff014ff0407b
clorin <clorin@student.42.fr>
2023-09-09 15:33:25 +0200

    essais json

--- essais/lecture.ml
+++ essais/lecture.ml
@@ -1,4 +1,17 @@
-open Yojson
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   lecture.ml                                         :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: clorin <clorin@student.42.fr>              +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2023/09/09 15:16:42 by clorin            #+#    #+#             *)
+(*   Updated: 2023/09/09 15:30:42 by clorin           ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+open Yojson.Basic
+
 let read_json_file file_name =
   try
     let json_content = Yojson.Basic.from_file file_name in
@@ -14,16 +27,15 @@ let read_json_file file_name =
 let () = 
   let json_content = read_json_file "exemple.json" in
 
-(*match json_content with
+match json_content with
 | `Assoc fields ->
   let nom = List.assoc "nom" fields in
   let age = List.assoc "age" fields in
   let ville = List.assoc "ville" fields in
 
   Printf.printf "Nom : %s\n" (Yojson.Basic.to_string nom);
-  Printf.printf "Âge : %d\n" (Yojson.Basic.to_int age);
+  Printf.printf "Âge : %s\n" (Yojson.Basic.to_string age);
   Printf.printf "Ville : %s\n" (Yojson.Basic.to_string ville);
 | _ ->
   Printf.eprintf "Le fichier JSON doit être un objet.\n";
-  raise Exit*)
-  print_endline "end"
+  raise Exit
